4e661fdbfca32fa5297e1fed692e98c5092125d0
Valentin Antonio de Gennaro <valentindegennaro@gmail.com>
2025-11-17 10:40:07 +0100

    17/11

---
+++ Apuntes/003- Realización de consultas/011- login en la base de datos.sql
@@ -0,0 +1,10 @@
+-- sudo mysql -u root -p
+
+USE clientes;
+
+SELECT nombre 
+FROM clientes;
+
+
+SELECT *
+FROM clientes;
---
+++ Apuntes/003- Realización de consultas/017- Resumen con conteo.sql
@@ -0,0 +1,9 @@
+-- sudo mysql -u root -p
+
+USE clientes;
+
+SELECT 
+COUNT(nombre)
+FROM clientes;
+
+
