b03008b162d63acc22c6f8d2561cbf51f2886bb2
Valentín Antonio de Gennaro <valentindegennaro@gmail.com>
2025-12-03 14:00:51 +0100

    1

---
+++ Apuntes/004-Tratamiento de datos/004-Insercion parte1.sql
@@ -0,0 +1,22 @@
+INSERT INTO equipos VALUES(
+	NULL,
+  "Levante",
+  "Valencia",
+  "Ciutat de Valencia",
+  1909,
+  10000000.00,
+  "https://levantecf.es"
+);
+
+SELECT * FROM equipos;
+
++----+---------------------+-----------+------------------------+---------+--------------+----------------------------------+
+| id | nombre              | ciudad    | estadio                | fundado | presupuesto  | web                              |
++----+---------------------+-----------+------------------------+---------+--------------+----------------------------------+
+|  1 | Real Madrid CF      | Madrid    | Santiago Bernabéu      |    1902 | 800000000.00 | https://www.realmadrid.com       |
+|  2 | FC Barcelona        | Barcelona | Spotify Camp Nou       |    1899 | 750000000.00 | https://www.fcbarcelona.es       |
+|  3 | Atlético de Madrid  | Madrid    | Cívitas Metropolitano  |    1903 | 450000000.00 | https://www.atleticodemadrid.com |
+|  4 | Valencia CF         | Valencia  | Mestalla               |    1919 | 150000000.00 | https://www.valenciacf.com       |
+|  5 | Levante             | Valencia  | Ciutat de Valencia     |    1909 |  10000000.00 | https://levantecf.es             |
++----+---------------------+-----------+------------------------+---------+--------------+----------------------------------+
+
