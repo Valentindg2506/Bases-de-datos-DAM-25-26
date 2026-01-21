d68459d7646c36632d66067b86fbb31310ba26a2
Valentindg2506 <valentindegennaro@gmail.com>
2026-01-21 12:39:54 +0100

    21/01/2026

---
+++ Apuntes/008-Proyectos/006-Repaso Mysql dump/004-Escribe.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+archivo = open("/var/www/html/GitHub/lineas.txt",'a')
+archivo.write("Esta es una linea\n")
+archivo.close()
---
+++ Apuntes/008-Proyectos/006-Repaso Mysql dump/005-Configuro cron.sh
@@ -0,0 +1,10 @@
+En el cron, añado esta linea:
+
+Terminal:
+crontab -e
+
+* * * * * /usr/bin/python3 /var/www/html/GitHub/004-escribe.py
+
+Y luego
+Control + O = Guardar
+Control + X = Salir
